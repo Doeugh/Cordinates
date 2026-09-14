de290d746274794da7b4e41a585dacec42c96c2a
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2026-09-14 12:13:30 -0300

    Added player input

--- Program.cs
+++ Program.cs
@@ -19,9 +19,58 @@ namespace Cordinates
             int itemX = 4;
             int itemY = 8;
 
+            while (true)
+            {
+                DrawGame(playerX, playerY, enemyX, enemyY, itemX, itemY);
+
+                bool quit = MovePlayer(ref playerX, ref playerY);
+
+                if (quit)
+                {
+                    break;
+                }
+            }
+        }
+
+        static void DrawGame(int playerX, int playerY, int enemyX, int enemyY, int itemX, int itemY)
+        {
+            Console.Clear();
+
             Console.WriteLine($"Player: ({playerX}, {playerY})");
             Console.WriteLine($"Enemy: ({enemyX}, {enemyY})");
             Console.WriteLine($"Item: ({itemX}, {itemY})");
+
+            Console.WriteLine();
+            Console.WriteLine("Use WASD to move");
+            Console.WriteLine("Q to quit");
+        }
+
+        static bool MovePlayer(ref int playerX, ref int playerY)
+        {
+            ConsoleKey key = Console.ReadKey(true).Key;
+
+            if (key == ConsoleKey.W)
+            {
+                playerY++;
+            }
+            else if (key == ConsoleKey.S)
+            {
+                playerY--;
+            }
+            else if (key == ConsoleKey.A)
+            {
+                playerX--;
+            }
+            else if (key == ConsoleKey.D)
+            {
+                playerX++;
+            }
+            else if (key == ConsoleKey.Q)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
