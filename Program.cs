04a480c891cdd1aac7b50d5a2907c9f065c13343
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2026-09-14 12:04:55 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cordinates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerX = 2;
+            int playerY = 3;
+
+            int enemyX = 7;
+            int enemyY = 5;
+
+            int itemX = 4;
+            int itemY = 8;
+
+            Console.WriteLine($"Player: ({playerX}, {playerY})");
+            Console.WriteLine($"Enemy: ({enemyX}, {enemyY})");
+            Console.WriteLine($"Item: ({itemX}, {itemY})");
+        }
+    }
+}
